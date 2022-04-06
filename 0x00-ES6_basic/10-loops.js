d6c831af6a49fb05873fab5dafcd4d70333c903e
root <resiatolaitete@gmail.com>
2022-04-06 15:55:17 +0000

    First commit for ES6 basics task

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const end = [];
+    for (const value of array) {
+    end.push(appendString + value);
+  }
+
+  return end;
+}
